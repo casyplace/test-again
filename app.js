561846d93c336b19091d7258515736dc78557ba8
root <casy@casy-place.com>
2018-08-10 14:29:23 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// siple async test for HTTP 200 response code supertwst
+'use strict';
+
+var express = require('express'),
+	app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/',function(req, res) {
+	res.render('home', {
+   });
+});
+
+app.listen(8080)
+module.exports.getApp = app;
---
+++ test/test.js
@@ -0,0 +1,13 @@
+// siple async test for HTTP 200 response code supertwst
+'use strict';
+
+var request = require("supertest"),
+	app = require("../app").getApp;
+
+describe('GET /', function(){
+  it('expexts HTTP response 200', function(done){
+	request(app)
+	 .get('/')
+	 .expect(200, done);
+	});
+});
